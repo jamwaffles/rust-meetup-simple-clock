b7d072cbeb78a16be92ff8620df67e9d2be8e099
James Waples <james@wapl.es>
2019-03-05 22:14:15 +0000

    Add SSD1306, print "Hello world!"

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,83 @@
 #![no_main]
 #![no_std]
 
-extern crate cortex_m;
-extern crate cortex_m_rt as rt;
 extern crate panic_semihosting;
-extern crate stm32f1xx_hal as hal;
 
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::hprintln;
+use embedded_graphics::{fonts::Font8x16, prelude::*};
 use rtfm::app;
+use ssd1306::{interface::I2cInterface, mode::GraphicsMode, Builder};
+use stm32f1xx_hal::{
+    gpio::{
+        gpiob::{PB8, PB9},
+        Alternate, OpenDrain,
+    },
+    i2c::{BlockingI2c, DutyCycle, Mode},
+    prelude::*,
+    stm32::I2C1,
+};
 
-#[app(device = stm32f1xx_hal::stm32f1::stm32f103)]
+type OledDisplay = GraphicsMode<
+    I2cInterface<BlockingI2c<I2C1, (PB8<Alternate<OpenDrain>>, PB9<Alternate<OpenDrain>>)>>,
+>;
+
+#[app(device = stm32f1xx_hal::stm32)]
 const APP: () = {
+    static mut DISPLAY: OledDisplay = ();
+
     #[init]
-    fn init() {
+    fn init() -> init::LateResources {
         hprintln!("init").unwrap();
+
+        let mut flash = device.FLASH.constrain();
+        let mut rcc = device.RCC.constrain();
+
+        let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+        let mut afio = device.AFIO.constrain(&mut rcc.apb2);
+
+        let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
+
+        let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+        let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+        let i2c = BlockingI2c::i2c1(
+            device.I2C1,
+            (scl, sda),
+            &mut afio.mapr,
+            Mode::Fast {
+                frequency: 400_000,
+                duty_cycle: DutyCycle::Ratio2to1,
+            },
+            clocks,
+            &mut rcc.apb1,
+            1000,
+            10,
+            1000,
+            1000,
+        );
+
+        let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+
+        display.init().expect("Failed to initialise display");
+        display.flush().expect("Failed to clear display");
+
+        hprintln!("init complete").unwrap();
+
+        init::LateResources { DISPLAY: display }
     }
 
-    #[idle]
+    #[idle(resources = [DISPLAY])]
     fn idle() -> ! {
         hprintln!("idle").unwrap();
 
-        // End the program
-        debug::exit(debug::EXIT_SUCCESS);
+        resources.DISPLAY.draw(
+            Font8x16::render_str("Hello world!")
+                .with_stroke(Some(1u8.into()))
+                .into_iter(),
+        );
+
+        resources.DISPLAY.flush().expect("Failed to update display");
 
         loop {}
     }
